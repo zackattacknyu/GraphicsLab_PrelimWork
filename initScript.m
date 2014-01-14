e8d5216cafad27821add112c8961f23ed2bbce43
zackattacknyu <zackattacknyu@gmail.com>
2014-01-13 16:01:48 -0800

    changed script after problem clarification

--- initScript.m
+++ initScript.m
@@ -1,38 +1,31 @@
-numPoints = 20; %number of rows
-range = 50; %the range of values will be integers from 0 to range
-data1 = floor(rand(1,numPoints)*range);
+%the range of randomly generated values will be 0 to range
+range = 50; 
+
+%the number of points generated between two instances of the sequence
+%   will be a random number between 
+%   minNumPoints and (minNumPoints + rangeNumPoints)
+minNumPoints = 30;
+rangeNumPoints = 50;
+
+numPointsVector = rand(1,3)*rangeNumPoints + minNumPoints;
+
+%generates the random data
+data1 = floor(rand(1,numPointsVector(1,1))*range);
+data2 = floor(rand(1,numPointsVector(1,2))*range);
+data3 = floor(rand(1,numPointsVector(1,3))*range);
 
 %the sequence [234 134 89 78] will be inserted in two different places
-nextNumPoints = floor(rand(1,3)*10);
 seqToFind = [234 201 198 255];
-data2 = floor(rand(1,nextNumPoints(1,1))*range);
-data3 = floor(rand(1,nextNumPoints(1,2))*range);
-data4 = floor(rand(1,nextNumPoints(1,3))*range);
-data5 = floor(rand(1,numPoints)*range);
+sizeSeq = size(seqToFind);
+numPointsSeq = sizeSeq(2); %number of points in seqToFind
 
-firstInstance = numPoints + nextNumPoints(1,1)
-secondInstance = firstInstance + 4 + nextNumPoints(1,2)
+firstInstance = numPointsVector(1,1)
+secondInstance = firstInstance + numPointsSeq + numPointsVector(1,2)
 
-%this is the created signal to analyze
-data = [data1 data2 seqToFind data3 seqToFind data4 data5];
+%this creates the signal that will be analyzed
+data = [data1 seqToFind data2 seqToFind data3]
 dataSize = size(data);
 numEntries = dataSize(2);
 
-corrs = [];
+%originalDataFFT = fft(originalData);
 
-for timeShift = 1:numEntries,
-
-    endIndex = numEntries - timeShift;
-    originalData = data(timeShift:numEntries);
-    shiftedData = data(1:endIndex);
-
-    %originalDataFFT = fft(originalData);
-    %shiftedDataFFT = fft(shiftedData);
-    %currentCorrelation = sum( xcorr(originalDataFFT,shiftedDataFFT) );
-
-    currentCorrelation = sum( xcorr(originalData,shiftedData) );
-
-    corrs = [corrs currentCorrelation];
-end
-
-plot(corrs,'r.')
