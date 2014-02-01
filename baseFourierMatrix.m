a5f068308114ea9bd3bbdd09edbd18574797f261
zackattacknyu <zackattacknyu@gmail.com>
2014-01-31 16:51:53 -0800

    changed the fourier matrix calculations

--- baseFourierMatrix.m
+++ baseFourierMatrix.m
@@ -1,14 +1,9 @@
 function [ fData ] = baseFourierMatrix( bigN )
 %FOURIERMATRIX Takes in N and returns the 
-%              base Fourier matrix used to reconstruct the signal
-%When reconstructing the signal we know that 
-%        x[t] = 1/N sum_{k=0}^{N-1} X_k exp(2*pi*i*t*k/N)
-%    We will then make an NxN matrix where each column is a different t
-%        and each row is a term in the summation
-%    This will construct the exp(2*pi*i*t*k/N) entries
+%              base Fourier matrix used to get the Fourier coefficients
 
 fData = [];
-omega = exp(2*pi*1i/bigN);
+omega = exp(-2*pi*1i/bigN);
 for kVal = 0:(bigN-1),
     currentRow = [];
     for tVal = 0:(bigN-1),
