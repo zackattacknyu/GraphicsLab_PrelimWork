ad5d24aa22ed51db2a060964df221db3abb05678
zackattacknyu <zackattacknyu@gmail.com>
2014-02-10 17:10:34 -0800

    constructed new frequency space for use

--- baseFourierMatrix.m
+++ baseFourierMatrix.m
@@ -3,15 +3,31 @@ function [ fData ] = baseFourierMatrix( bigN )
 %              base Fourier matrix used to get the Fourier coefficients
 
 fData = [];
-omega = exp(-2*pi*1i/bigN);
-for kVal = 0:(bigN-1),
+%multiple = -2;
+%omega = exp(-2*pi*multiple*1i/bigN);
+for kVal = 0:((bigN-1)/2),
     currentRow = [];
-    for tVal = 0:(bigN-1),
-        value = omega^(tVal*kVal);
+    for tVal = 0:(bigN-2),
+        %value = omega^(tVal*kVal);
+        value = cos(tVal*kVal*2*pi/(bigN-1));
         currentRow = [currentRow value];
     end
+    currentRow = currentRow/norm(currentRow);
     fData = [fData;currentRow];
 end
+
+for kVal = 1:((bigN-1)/2),
+    currentRow = [];
+    for tVal = 0:(bigN-2),
+        %value = omega^(tVal*kVal);
+        value = -1*cos(tVal*kVal*2*pi/(bigN-1) + pi/2);
+        currentRow = [currentRow value];
+    end
+    currentRow = currentRow/norm(currentRow);
+    fData = [fData;currentRow];
+end
+
+fData*transpose(fData)
 
 end
 
--- initScript.m
+++ initScript.m
@@ -1,8 +1,8 @@
 %TEST CODE: makes a small array of data
 %data = [2 4 7 8 3 4 9 10 23 12 3 9 10 12 13 1 3 4 2 9 10 12 13 5 4];
 %data = [2 3 4 4 4 1 3 4 2 4 4 4 5 4];
-%data = [2 10 12 13 14 15 20 34 45 67 2 10 12 13 14 15 20];
-data = [2 3 15 2 3];
+data = [2 10 12 13 14 15 20 34 45 67 2 10 12 13 14 15 20];
+%data = [2 3 15 2 3];
 
 dataSize = size(data);
 bigN = dataSize(2);
